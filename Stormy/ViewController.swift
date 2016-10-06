8485e4028863a896757d4268fabecce8fa016848
paulthegeek <pj2454@gmail.com>
2016-10-05 22:55:11 -0600

    Pulling JSON data from Forecast. Totally wrong never do this.

--- Stormy/ViewController.swift
+++ Stormy/ViewController.swift
@@ -18,6 +18,8 @@ class ViewController: UIViewController {
     @IBOutlet weak var refreshButton: UIButton!
     @IBOutlet weak var activityIndicator: UIActivityIndicatorView!
     
+    private let forecastAPIKey = "2bd675f3096bb54a1f18c26df85b7bff"
+    
 
     override func viewDidLoad() {
         super.viewDidLoad()
@@ -26,6 +28,20 @@ class ViewController: UIViewController {
         let currentWeather = CurrentWeather(temperature: 56.0, humidity: 1.0, precipitationProbability: 1.0,
                                             summary: "Wet and rainy", icon: icon)
         display(currentWeather)
+        
+        let baseURL = NSURL(string: "https://api.darksky.net/forecast/\(forecastAPIKey)/")
+        let forecastURL = NSURL(string: "37.8267,-122.4233", relativeToURL: baseURL)
+        
+        let configuration = NSURLSessionConfiguration.defaultSessionConfiguration()
+        let session = NSURLSession(configuration: configuration)
+        
+        let request = NSURLRequest(URL: forecastURL!)
+        
+        let dataTask = session.dataTaskWithRequest(request) {data, response, error in
+            print(data!)
+        }
+        
+        dataTask.resume()
     }
 
     override func didReceiveMemoryWarning() {

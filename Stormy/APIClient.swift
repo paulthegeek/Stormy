91f9ed6ab857e583976789969ff14e897d34f209
paulthegeek <pj2454@gmail.com>
2016-10-18 07:57:32 -0600

    More API Client

--- Stormy/APIClient.swift
+++ Stormy/APIClient.swift
@@ -8,12 +8,17 @@
 
 import Foundation
 
+// Add three letter prefixes to name to prevent collosions
+public let COMNetworkingErrorDomain = "com.companyName.Stromy.NetworkingError"
+public let MissingHTTPResponseError: Int = 10
+
 typealias JSON = [String : AnyObject]
 typealias JSONTaskCompletion = (JSON?, NSHTTPURLResponse? , NSError?) -> Void
 typealias JSONTask = NSURLSessionDataTask
 
 enum APIResult<T> {
     case Success(T)
+    // Can use Swift error type and Obj-C NSError type
     case Failure(ErrorType)
 }
 
@@ -23,6 +28,26 @@ protocol APIClient {
     
     init(config: NSURLSessionConfiguration)
     
+    // Take a request object containing the URL that we want to fetch data from
     func JSONTaskWithRequest(request: NSURLRequest, completion: JSONTaskCompletion) -> JSONTask
+    
+    // 
     func fetch<T>(request: NSURLRequest, parse: JSON -> T?, completion: APIResult<T> -> Void)
+}
+
+extension APIClient {
+    func JSONTaskWithRequest(request: NSURLRequest, completion: JSONTaskCompletion) -> JSONTask {
+        let task = session.dataTaskWithRequest(request) { data, response, error in
+        }
+        guard let HTTPResponse = response as? NSHTTPURLResponse else {
+            let userInfo = [ NSLocalizedDescriptionKey: NSLocalizedString("Missing HTTP Response", comment: "") ]
+            
+            let error = NSError(domain: COMNetworkingErrorDomain, code: MissingHTTPResponseError, userInfo: userInfo)
+            
+            completion(nil, nil, error)
+            
+            return
+        }
+        return task
+    }
 }
